be8d06e5cba0235c4278ca94d5a1b782e544d70a
marijnkoolen <marijn.koolen@gmail.com>
2020-01-30 09:44:42 +0000

    Clarify variable names

--- republic/download/republic_data_downloader.py
+++ republic/download/republic_data_downloader.py
@@ -28,32 +28,34 @@ def get_inventory_data(url: str) -> Union[bytes, None]:
         return None
 
 
-def store_inventory_data(inventory_data: bytes, inventory_num: int, data_type: str, inventory_config: dict):
-    out_file = get_output_filename(inventory_num, data_type, inventory_config)
+def store_inventory_data(inventory_data: bytes, inventory_num: int, ocr_type: str, inventory_config: dict):
+    out_file = get_output_filename(inventory_num, ocr_type, inventory_config)
+    print(f'\tStoring inventory {inventory_num} {ocr_type} data to {out_file}')
     with open(out_file, 'wb') as fh:
         fh.write(inventory_data)
 
 
-def get_output_filename(inventory_num: int, data_type: str, inventory_config: dict) -> str:
+def get_output_filename(inventory_num: int, ocr_type: str, inventory_config: dict) -> str:
     data_dir = None
-    if data_type == "hocr" or data_type == "pagexml":
-        data_dir = os.path.join(inventory_config["base_dir"], data_type)
+    if ocr_type == "hocr" or ocr_type == "pagexml":
+        data_dir = os.path.join(inventory_config["base_dir"], ocr_type)
     if data_dir:
         return os.path.join(data_dir, f"{inventory_num}.zip")
     else:
         ValueError("Unknown data type. Must be either 'hocr' or 'pagexml'.")
 
 
-def download_inventory(es: Elasticsearch, inventory_num: int, data_type: str, inventory_config: dict):
+def download_inventory(es: Elasticsearch, inventory_num: int, ocr_type: str, inventory_config: dict):
     uuid = get_inventory_uuid(es, inventory_num, inventory_config)
     urls = get_download_urls(uuid)
-    if data_type == "hocr":
+    if ocr_type == "hocr":
         url = urls["hocr_url"]
-    elif data_type == "pagexml":
+    elif ocr_type == "pagexml":
         url = urls["pagexml_url"]
+        print('download url:', url)
     else:
         raise ValueError("Unknown data type. Must be either 'hocr' or 'pagexml'.")
     inventory_data = get_inventory_data(url)
     if inventory_data:
-        store_inventory_data(inventory_data, inventory_num, data_type, inventory_config)
+        store_inventory_data(inventory_data, inventory_num, ocr_type, inventory_config)
 
