be66925969e8aa3eda27d2d36470ce75c39346d9
HDJ <hayco@users.noreply.github.com>
2020-05-20 14:16:40 +0200

    Fetch externalIDs from Pergamon Images

--- textrepo/import-pim.py
+++ textrepo/import-pim.py
@@ -15,12 +15,60 @@ if (len(sys.argv) <= 1):
 with open(sys.argv[1]) as ymlfile:
 	config = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
-REPO = config['repo']
+PIM_AUTH = config['pim_auth']
+AUTH_HDR = {'Authorization': PIM_AUTH}
+
+PIM_BASE = config['pim_base']
+PIM_DOCS = PIM_BASE + config['pim_docs']
+
+PIM_IMGS = PIM_DOCS + config['pim_imgs']
+
+TXT_REPO = config['txt_repo']
 
 
+def fetch_document_image_set(session):
+	req = requests.Request('GET', PIM_DOCS, headers=AUTH_HDR).prepare()
+	resp = session.send(req)
+	resp.raise_for_status()
+	if resp.status_code == 200:
+		print("Got document image set")
+	return resp.json()
+
+def is_republic(doc):
+	if doc['elasticSearchIndex'] == None:
+		return False
+	return doc['elasticSearchIndex']['name'] == 'republic'
+
+def fetch_image_set(session, uuid):
+	url = PIM_IMGS.format(id = uuid)
+	print("Fetching: " + url)
+	req = requests.Request('GET', url=url, headers=AUTH_HDR).prepare()
+	resp = session.send(req)
+	resp.raise_for_status()
+	return resp.json()
+
+def extract_id(uri):
+	"""Extract externalId from remoteuri"""
+	parts = uri.split('/')
+	num = len(parts)
+	assert(num > 1)
+	file = parts[num - 1]
+	return os.path.splitext(file)[0]
 
 def main():
-	print("Repo: " + REPO)
+	print("PIM_DOCS: " + PIM_DOCS)
+	print("PIM_IMGS: " + PIM_IMGS)
+	print("Text Repo: " + TXT_REPO)
+
+	with requests.Session() as session:
+		docs = fetch_document_image_set(session)
+		republic_docs = list(filter(is_republic, docs))
+		for doc in republic_docs:
+			img_set = fetch_image_set(session, doc['uuid'])
+			for img in img_set:
+				externalId = extract_id(img['remoteuri'])
+				print("externalId: " + externalId)
+			sys.exit(0) # early exit during development
 
 if __name__ == "__main__":
 	main()
