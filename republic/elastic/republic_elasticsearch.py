d1d2e3aca3fa347b6a128625805da9703f13b45f
marijnkoolen <marijn.koolen@gmail.com>
2020-11-05 14:11:19 +0000

    Fix local port

--- republic/elastic/republic_elasticsearch.py
+++ republic/elastic/republic_elasticsearch.py
@@ -26,6 +26,8 @@ from settings import set_elasticsearch_config
 def initialize_es(host_type: str = 'internal', timeout: int = 10) -> Elasticsearch:
     republic_config = set_elasticsearch_config(host_type)
     es_config = republic_config['elastic_config']
+    print('es_config:')
+    print(es_config)
     if es_config['url_prefix']:
         es_republic = Elasticsearch([{'host': es_config['host'],
                                       'port': es_config['port'],
