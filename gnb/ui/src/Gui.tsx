5abd221c5fc959c42a02693fd8b444b0255e6e4e
basl <basleenknegt@gmail.com>
2021-04-16 09:40:41 +0200

    Remove testdata, clean up Gui.tsx

--- gnb/ui/src/Gui.tsx
+++ gnb/ui/src/Gui.tsx
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React from 'react';
 import GnbElasticClient from "./elastic/GnbElasticClient";
 import ContextProvider from "./ContextProvider";
 import Version from "./Version";
@@ -16,27 +16,27 @@ type GuiProps = {
  */
 export default function Gui(props: GuiProps) {
 
-  const svgRef = useRef(null);
-
   return (
     <>
       <div className="gui container-fluid">
         <ContextProvider client={props.client}>
+
           <div className="row mt-3 mb-3">
             <div className="col">
               <h1>GNB <small className="text-muted">Governance, Netwerken en Besluitvorming in de Staten-Generaal </small></h1>
             </div>
           </div>
+
           <Search/>
           <ResolutionViewer/>
           <Views/>
           <ViewComposer/>
+
         </ContextProvider>
       </div>
       <Version />
     </>
   );
-
 
 }
 
