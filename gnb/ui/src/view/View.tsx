861cb83d7449fceffc1503d696c7fe4f9c5c9ace
basl <basleenknegt@gmail.com>
2021-05-14 13:36:12 +0200

    Update views when plot type changes

--- gnb/ui/src/view/View.tsx
+++ gnb/ui/src/view/View.tsx
@@ -5,6 +5,7 @@ import {useResolutionContext} from "../resolution/ResolutionContext";
 import {EntityViewer} from "./entity/EntityViewer";
 import {ViewType} from "./model/ViewType";
 import {toStr, ViewEntityType} from "./model/ViewEntityType";
+import {usePlotContext} from "../common/plot/PlotContext";
 
 type ViewProps = {
   entity: ViewEntityType,
@@ -17,11 +18,12 @@ export default function View(props: ViewProps) {
   const deleteRef = useRef(null)
   const isHovering = useHover(deleteRef);
   const {resolutionState} = useResolutionContext();
+  const {plotState} = usePlotContext();
 
   function memoBy() {
     const resolutions = resolutionState.resolutions.map(r => r.ids).join(',');
     const strEntity = toStr(props.entity);
-    return JSON.stringify([resolutions, props.type, strEntity]);
+    return JSON.stringify([resolutions, props.type, strEntity, plotState.type]);
   }
 
   return <div className="row mt-3">
