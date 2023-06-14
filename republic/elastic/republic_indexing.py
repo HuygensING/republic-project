560e4a94b58cdae99c8b1264f799f5245aedb82a
Marijn Koolen <marijn.koolen@gmail.com>
2023-06-14 14:15:40 +0200

    Fixed inventory_id property name

--- republic/elastic/republic_indexing.py
+++ republic/elastic/republic_indexing.py
@@ -130,7 +130,7 @@ class Indexer:
 
     def index_scan(self, scan: pdm.PageXMLScan):
         if 'inventory_id' not in scan.metadata:
-            scan.metadata['inventory'] = f"{scan.metadata['series_name']}_{scan.metadata['inventory_num']}"
+            scan.metadata['inventory_id'] = f"{scan.metadata['series_name']}_{scan.metadata['inventory_num']}"
         self.index_doc(index=self.config['scans_index'], doc_id=scan.id, doc_body=scan.json)
 
     def index_page(self, page: pdm.PageXMLPage):
