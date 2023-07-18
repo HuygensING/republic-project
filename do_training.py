8d71154f137c4bc514c54735950762230b8e005c
marijnkoolen <marijn.koolen@gmail.com>
2023-07-18 12:01:07 +0000

    Fix bugs in do_training.py

--- do_training.py
+++ do_training.py
@@ -4,7 +4,7 @@ from republic.tag.train import prep_training
 from republic.tag.train import train
 
 
-ENTITY_TYPES = {'HOE', 'PER', 'COM', 'ORG', 'LOC', 'DAT', 'REF'}
+ENTITY_TYPES = {'HOE', 'PER', 'COM', 'ORG', 'LOC', 'DAT', 'RES'}
 
 
 def train_entity_tagger(layer_name: str, train_size: float = 1.0, hidden_size=256,
@@ -19,27 +19,32 @@ def parse_args():
     argv = sys.argv[1:]
     # Define the getopt parameters
     try:
-        opts, args = getopt.getopt(argv, 'l:s:r:m',
-                                   ['layers=', 'train_size=', 'learning_rate=', 'mini_batch_size='])
+        opts, args = getopt.getopt(argv, 'e:l:s:r:m',
+                                   ['epochs=', 'layers=', 'train_size=', 'learning_rate=', 'mini_batch_size='])
         layers = ['single_layer']
         train_size = 1.0
         learing_rate = 0.05
         mini_batch_size = 32
+        max_epochs = 10
         for opt, arg in opts:
-            if opt in {'-l', '--layer'}:
-                layer = arg
-                if ':' in layer:
-                    layers = layer.split(':')
+            if opt in {'-e', '--epochs'}:
+                max_epochs = int(arg)
+            if opt in {'-l', '--layers'}:
+                layers = arg
+                if ':' in layers:
+                    layers = layers.split(':')
                 else:
                     layers = [layer]
                 assert all([layer in ENTITY_TYPES for layer in layers])
             if opt in {'-s', '--train_size'}:
                 train_size = int(arg)
             if opt in {'-r', '--learning_rate'}:
-                learing_rate = int(arg)
+                learing_rate = float(arg)
             if opt in {'-m', '--mini_batch_size'}:
-                mini_batch_size = arg
-        return layers, train_size, learing_rate, mini_batch_size
+                print(arg)
+                mini_batch_size = int(arg)
+        print('training layers:', layers)
+        return layers, train_size, learing_rate, mini_batch_size, max_epochs
     except getopt.GetoptError:
         # Print something useful
         print('usage: add.py -s <start_year> -e <end_year> -i <indexing_step> -n <num_processes')
@@ -47,10 +52,10 @@ def parse_args():
 
 
 def main():
-    layers, train_size, learing_rate, mini_batch_size = parse_args()
+    layers, train_size, learing_rate, mini_batch_size, max_epochs = parse_args()
     for layer in layers:
         print(f'training layer {layer}')
-        train_entity_tagger(layer_name=layer, train_size=train_size, mini_batch_size=mini_batch_size)
+        train_entity_tagger(layer_name=layer, train_size=train_size, mini_batch_size=mini_batch_size, max_epochs=max_epochs)
 
 
 if __name__ == "__main__":
