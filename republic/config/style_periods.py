32bdcce3e9fa7c7b96a2e9c43d12f40925173ed6
Marijn Koolen <marijn.koolen@gmail.com>
2023-11-02 09:44:27 +0100

    Improve page splitting and session parsing
    - Fix bug with reading headers in ResolutionSentences class
    - Add logic for reading from TSV
    - Fix ignorecase bug in checking session date lines
    - Improve page splitting with better book width estimation

---
+++ republic/config/style_periods.py
@@ -0,0 +1,14 @@
+
+style_periods = [
+    {'start': 1597, 'end': 1628},
+    {'start': 1629, 'end': 1637},
+    {'start': 1638, 'end': 1650},
+    {'start': 1652, 'end': 1670},
+    {'start': 1671, 'end': 1692},
+    {'start': 1693, 'end': 1702},
+    {'start': 1705, 'end': 1716},
+    {'start': 1717, 'end': 1735},
+    {'start': 1736, 'end': 1764},
+    {'start': 1765, 'end': 1786},
+    {'start': 1787, 'end': 1796}
+]
