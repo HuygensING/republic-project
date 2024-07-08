768d95cc669b0f405d34bfd54dbeaf222769d54b
marijnkoolen <marijn.koolen@gmail.com>
2024-07-08 11:27:35 +0000

    Add printed to HTR indexing

--- do_all_indexing.py
+++ do_all_indexing.py
@@ -2,6 +2,7 @@ import tarfile
 import datetime
 import logging
 import xml
+from collections import defaultdict
 
 from elasticsearch.exceptions import ElasticsearchException
 
@@ -11,10 +12,12 @@ import republic.parser.pagexml.republic_pagexml_parser as pagexml_parser
 rep_es = initialize_es(host_type="internal", timeout=60)
 
 
-def index_scan(inv_file, file_content, inv_metadata):
+def index_scan(inv_file, file_content, inv_metadata, inv_file_count: int = 0):
         try:
             scan = pagexml_parser.get_scan_pagexml(inv_file, pagexml_data=file_content, inv_metadata=inv_metadata)
-            # print(scan.id, scan.stats)
+            print(f"inv {scan.metadata['inventory_num']}, scan {inv_file_count} - indexing scan {scan.id} with {scan.stats['words']} words")
+            prov_url = rep_es.post_provenance([inv_file], [scan.id], 'Loghi', 'scans')
+            scan.metadata['provenance_url'] = prov_url
             rep_es.index_scan(scan)
         except (xml.parsers.expat.ExpatError, ValueError, IndexError, KeyError, TypeError, ElasticsearchException, AttributeError) as err:
             print(f'Error parsing file {inv_file} - {err}')
@@ -22,7 +25,7 @@ def index_scan(inv_file, file_content, inv_metadata):
             pass
 
 
-def main():
+def do_htr_handwritten():
     all_file = 'data/pagexml/loghi-htr.tgz'
     file_count = 0
     inv_metadata = None
@@ -46,6 +49,49 @@ def main():
                 print(file_count, 'pagexml files processed')
                 logging.info(f'{file_count} pagexml files processed')
     print('Finished!')
+
+
+def do_htr_printed():
+    all_file = 'data/pagexml/pagexml-loghi_htr-2022-invs_3760-3864.tgz'
+    file_count = 0
+    inv_file_count = defaultdict(int)
+    inv_metadata = None
+    do_index = False
+    with tarfile.open(all_file, 'r:gz') as fh:
+        print('opened tarfile')
+        for file_info in fh:
+            if file_info.isdir():
+                print('Dir:', file_info.name)
+                logging.info(f'extracting scans from dir {file_info.name}')
+                inv_num = int(file_info.name)
+                inv_metadata = rep_es.retrieve_inventory_metadata(inv_num)
+                continue
+            elif file_info.name.endswith('.xml') is False:
+                logging.info(f'skipping non-XML file {file_info.name}')
+                continue
+            elif file_info.name.startswith(f"{inv_num}/._"):
+                continue
+            # print('file_content:', file_content)
+            # print('file_info.name:', file_info.name)
+            # print('inv_metadata:', inv_metadata)
+            # print('inv_num:', inv_num, '\tdo_index:', do_index)
+            inv_file_count[inv_num] += 1
+            if inv_num == 3848:
+                do_index = True
+            if do_index is True:
+                file_reader = fh.extractfile(file_info)
+                file_content = file_reader.read()
+                index_scan(file_info.name, file_content, inv_metadata, inv_file_count[inv_num])
+            file_count += 1
+            if file_count % 1000 == 0:
+                print(file_count, 'pagexml files processed')
+                logging.info(f'{file_count} pagexml files processed')
+    print('Finished!')
+
+
+def main():
+    # do_htr_handwritten()
+    do_htr_printed()
 
 
 if __name__ == "__main__":
